39a35856ac3f164944fc4bc16f144e590808464a
Mario Mendes <mario@Marios-MacBook-Air.local>
2014-06-14 23:21:20 +0100

    add watcher-spawn-parse

---
+++ watcher-spawn-parse.js
@@ -0,0 +1,26 @@
+//This code captures data from an EventEmitter
+//http://nodejs.org/api/events.html
+
+"use strict";
+const
+	fs = require('fs'),
+	spawn = require('child_process').spawn,
+	filename = process.argv[2];
+
+if (!filename) {
+	throw Error ("A file to watch must be specified");
+}
+
+fs.watch(filename, function(){
+	let ls = spawn('ls', ['-lh', filename]),
+		output = '';
+	ls.stdout.on('data', function(chunk){
+		output += chunk.toString ();
+		});
+
+	ls.on('close', function () {
+		let parts = output.split(/|s+/);
+		console.dir([parts[0], parts[4], parts[8]]);
+		});
+});
+console.log("Now watching " + filename + " for changes ...");
