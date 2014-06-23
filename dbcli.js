1c83e563621c48c1f2771a66705be7a967daa5a1
Mario Mendes <mario.c.mendes1985@gmail.com>
2014-06-23 20:42:28 +0100

    add database client

---
+++ dbcli.js
@@ -0,0 +1,17 @@
+//working with couchDB
+//making RESTful requests
+
+#!/usr/bin/env node --harmony
+const
+	request = require('request'),
+	option = {
+		method: process.argv[2] || 'GET',
+		url: 'http://localhost:5984/' + (process.argv[3] || '')
+	};
+request(options, function(err, res, body){
+	if(err) {
+		throw Error (err);
+	} else {
+		console.log(res.statusCode, JSON.parse(body));
+	}
+});
